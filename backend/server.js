e189e22bf84ef340b61df057663bec849274435f
lakshmirajvagu <lakshmirajvagu@gmail.com>
2026-02-01 19:16:06 +0530

    final deployment

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 
 /* -------------------- MIDDLEWARE -------------------- */
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://fullstacksmartsettle-d2ji.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
@@ -51,7 +51,7 @@ const server = http.createServer(app);
 
 export const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://fullstacksmartsettle-d2ji.vercel.app",
     methods: ["GET", "POST"]
   }
 });
